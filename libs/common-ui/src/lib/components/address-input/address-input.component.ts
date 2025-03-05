252708102b70aa146b972491f1b2182e39e9e200
Oksana-Kaz <oksanakazjob@gmail.com>
2025-03-05 11:36:18 -0500

    created address-input component

---
+++ libs/common-ui/src/lib/components/address-input/address-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tt-address-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './address-input.component.html',
+  styleUrl: './address-input.component.css'
+})
+export class AddressInputComponent {
+
+}
