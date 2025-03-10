f90c00365523b15e144821e1e84b87330ff7f35a
Oksana-Kaz <oksanakazjob@gmail.com>
2025-03-10 14:14:22 -0400

    created address-input component, version 1 controlValue

--- libs/auth/src/lib/feature-login/login-page/login-page.component.ts
+++ libs/auth/src/lib/feature-login/login-page/login-page.component.ts
@@ -1,21 +1,23 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, forwardRef, inject, signal } from '@angular/core';
 import {
   FormControl,
-  FormGroup,
+  FormGroup, NG_VALUE_ACCESSOR,
   ReactiveFormsModule,
-  Validators,
+  Validators
 } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '@tt/data-access';
-
+import { TtInputComponent } from '@tt/common-ui';
 
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, TtInputComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
+
+
 })
 export class LoginPageComponent {
   authService = inject(AuthService);
@@ -24,13 +26,19 @@ export class LoginPageComponent {
   isPasswordVisible = signal<boolean>(false);
 
   form = new FormGroup({
-    username: new FormControl(null, Validators.required),
+    username: new FormControl('USERNAME', Validators.required),
     password: new FormControl(null, Validators.required),
   });
 
+  ngOnInit() {
+    this.form.valueChanges.subscribe(val => {
+      console.log(val);
+    })
+    // this.form.controls.username.disable()
+  }
   onSubmit() {
     if (this.form.valid) {
-      console.log(this.form.value)
+      console.log(this.form.value);
       // @ts-ignore
       this.authService.login(this.form.value).subscribe((res) => {
         this.router.navigate(['']);
--- libs/common-ui/src/lib/components/address-input/address-input.component.ts
+++ libs/common-ui/src/lib/components/address-input/address-input.component.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef, inject, signal } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { TtInputComponent } from './../tt-input/tt-input.component';
+import { DadataService } from '../../data/services/dadata.service';
+import { debounceTime, switchMap, tap } from 'rxjs';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+
 
 @Component({
   selector: 'tt-address-input',
   standalone: true,
-  imports: [],
+  imports: [TtInputComponent, ReactiveFormsModule, AsyncPipe, JsonPipe],
   templateUrl: './address-input.component.html',
-  styleUrl: './address-input.component.css'
+  styleUrl: './address-input.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => AddressInputComponent),
+    },
+  ],
 })
-export class AddressInputComponent {
+export class AddressInputComponent implements ControlValueAccessor {
+  innerSearchControl = new FormControl();
+  #dadataService = inject(DadataService);
 
+  isDropdownOpened = signal<boolean>(true);
+
+  suggestions$ = this.innerSearchControl.valueChanges.pipe(
+    debounceTime(500),
+    switchMap((val) => {
+      return this.#dadataService.getSuggestion(val)
+        .pipe(
+          tap(res  => {
+            this.isDropdownOpened.set(!!res.length)
+          })
+        )
+    })
+  );
+
+  writeValue(city: string | null): void {
+    this.innerSearchControl.patchValue(city, {
+      emitEvent: false
+    })
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+  }
+
+  onChange(value: any): void {
+  }
+
+  onTouched = (): void => {
+  }
+
+
+  onSuggestionPick(city: string) {
+    this.isDropdownOpened.set(false)
+    this.innerSearchControl.patchValue(city, {
+      emitEvent: false
+    })
+    this.onChange(city);
+
+  }
 }
